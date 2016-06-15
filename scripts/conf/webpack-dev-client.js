90c47823c983bb98dec07dbcd5073dc8727ee66e
dengjiayao <dnxbf321@gmail.com>
2016-06-15 11:26:44 +0800

    fix hot module reload

--- scripts/conf/webpack-dev-client.js
+++ scripts/conf/webpack-dev-client.js
@@ -1,5 +1,6 @@
 require('eventsource-polyfill')
-var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+
+var hotClient = require('webpack-hot-middleware/client?path=http://127.0.0.1:8080/__webpack_hmr&noInfo=true&reload=true')
 
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
