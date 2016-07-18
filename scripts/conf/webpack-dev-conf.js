715fb3e10f49afa2de61b24300b1134ccae1a25c
dengjiayao <dnxbf321@gmail.com>
2016-07-18 13:30:35 +0800

    fix code

--- scripts/conf/webpack-dev-conf.js
+++ scripts/conf/webpack-dev-conf.js
@@ -1,5 +1,4 @@
 var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var merge = require('webpack-merge')
 var baseConfig = require('./webpack-base-conf')
 
