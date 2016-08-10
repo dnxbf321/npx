a521baff9da90a41e998483f09726ac317ca7e09
dengjiayao <dnxbf321@gmail.com>
2016-08-10 17:08:02 +0800

    improve init

--- scripts/task/init.js
+++ scripts/task/init.js
@@ -1,8 +1,20 @@
 var clone = require('git-clone')
 var colors = require('colors')
+var glob = require('glob')
 
 module.exports = function() {
-  clone('https://github.com/dnxbf321/npx-template.git', process.cwd(), function() {
-    console.log(colors.bgGreen('[task init]'), 'done')
-  })
+  var found = glob.sync('@(*|.*)')
+
+  if (found.length) {
+    console.log(colors.bgRed('[task init]'), 'this folder is not empty')
+  } else {
+    console.log(colors.bgGreen('[task init]'), 'wait seconds to download the template')
+    clone('https://github.com/dnxbf321/npx-template.git', process.cwd(), function(err) {
+      if (err) {
+        console.log(colors.bgRed('[task init]'), err)
+      } else {
+        console.log(colors.bgGreen('[task init]'), 'done')
+      }
+    })
+  }
 }
