f4e552e1f0b1d789d8da9e5a69d4f801713ca5a4
dengjiayao <dnxbf321@gmail.com>
2016-08-18 09:54:03 +0800

    use official update-notifier

--- scripts/util/pkg-update.js
+++ scripts/util/pkg-update.js
@@ -2,6 +2,9 @@ var updateNotifier = require('update-notifier')
 var pkg = require('../../package.json')
 
 module.exports = function() {
+  delete process.env.npm_config_username
+  delete process.env.npm_package_name
+  delete process.env.npm_config_heading
   var notifier = updateNotifier({
     pkg: pkg,
     updateCheckInterval: 2 * 1000
