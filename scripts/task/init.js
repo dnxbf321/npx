cb1d5f4f70d92f7c64593135e93e59c2fe92b774
dengjiayao <dnxbf321@gmail.com>
2016-08-18 15:33:50 +0800

    init 任务后删除 .git

--- scripts/task/init.js
+++ scripts/task/init.js
@@ -1,6 +1,8 @@
 var clone = require('git-clone')
 var colors = require('colors')
 var glob = require('glob')
+var rimraf = require('rimraf')
+var path = require('path')
 
 module.exports = function() {
   var found = glob.sync('@(*|.*)')
@@ -13,6 +15,7 @@ module.exports = function() {
       if (err) {
         console.log(colors.bgRed('[task init]'), err)
       } else {
+        rimraf.sync(path.join(process.cwd(), '.git'))
         console.log(colors.bgGreen('[task init]'), 'done')
       }
     })
