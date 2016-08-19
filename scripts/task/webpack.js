819c86d379abf57b5d923ced79bb0e7345ab6133
dengjiayao <dnxbf321@gmail.com>
2016-08-19 17:32:59 +0800

    build 显示 webpack 进度

--- scripts/task/webpack.js
+++ scripts/task/webpack.js
@@ -1,13 +1,17 @@
 var webpack = require('webpack')
+var ProgressPlugin = require('webpack/lib/ProgressPlugin')
 var colors = require('colors')
-var getDevConf = require('../webpack-conf/webpack-dev-conf')
 var getProdConf = require('../webpack-conf/webpack-prod-conf')
 var aliasEnv = require('../util/alias-env')
 
 module.exports = function(env) {
   env = aliasEnv(env)
   var conf = getProdConf(env)
-  webpack(conf, (err, stats) => {
+  var compiler = webpack(conf)
+  compiler.apply(new ProgressPlugin((percentage, msg) => {
+    console.log(colors.bgGreen('[task webpack]'), parseInt(percentage * 100) + '%', msg)
+  }))
+  compiler.run((err, stats) => {
     if (err) {
       console.log(colors.bgRed('[task webpack]'), err)
     } else {
