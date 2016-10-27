0a4cca179242a5851f9f79111a6493dbd409a741
dengjiayao <dnxbf321@gmail.com>
2016-10-27 08:26:49 +0800

    fix babel-asset task

--- scripts/task/babel-asset.js
+++ scripts/task/babel-asset.js
@@ -1,66 +1,33 @@
-import { transformFile } from 'babel-core'
-import mkdirp from 'mkdirp'
-import rimraf from 'rimraf'
+import webpack from 'webpack'
 import colors from 'colors'
 import leftPad from 'left-pad'
 import glob from 'glob'
+import rimraf from 'rimraf'
 import path from 'path'
-import fs from 'fs'
+import getBabelAssetconf from '../webpack-conf/webpack-babel-asset-conf'
 import aliasEnv from '../util/alias-env'
-import babelrc from '../util/babelrc'
-import getWebpackDefinition from '../webpack-conf/webpack-definition'
-
-var projectRoot = process.cwd()
 
 export default (env) => {
-  env = aliasEnv(env)
-
-  var definition = getWebpackDefinition(env)
-
-  babelrc.plugins.push([require.resolve('babel-plugin-transform-define'), definition])
-  babelrc.minified = env === 'production'
-  babelrc.sourceMaps = env !== 'production' ? 'inline' : false
-
-  var assets = glob.sync('**/*.bl.js', {
-    cwd: path.join(projectRoot, 'client/asset')
-  })
-
-  var promises = []
-
-  assets.forEach((js, idx) => {
-
-    let transformer = new Promise((resolve, reject) => {
-      transformFile(path.join(projectRoot, 'client/asset', js), babelrc, (err, result) => {
-        if (err) {
-          console.log(colors.bgRed(`[task ${leftPad('babel-asset', 12)}]`), err)
-          reject()
-        }
-
-        let dist = path.join(projectRoot, 'client/dist/static')
-        rimraf(path.join(dist, js), {}, () => {
-        })
-        mkdirp(dist, (err) => {
-          if (err) {
-            console.log(colors.bgRed(`[task ${leftPad('babel-asset', 12)}]`), err)
-            reject()
-          } else {
-            fs.writeFile(path.join(dist, js).replace('.bl.js', '.js'), result.code, (err) => {
-              if (err) {
-                console.log(colors.bgRed(`[task ${leftPad('babel-asset', 12)}]`), err)
-                reject()
-              } else if (idx === assets.length - 1) {
-                console.log(colors.bgGreen(`[task ${leftPad('babel-asset', 12)}]`), 'done')
-                resolve()
-              }
-            })
-          }
-        })
-      })
+  // 删除 dist 下的 .bl.js 文件
+  var files = glob.sync(path.join(process.cwd(), 'client/dist/static/**/*.bl.js'))
+  files.forEach((it) => {
+    rimraf(it, {}, () => {
     })
-    promises.push(transformer)
-
   })
 
-  return Promise.all(promises)
-
+  // 打包处理 .bl.js 文件
+  env = aliasEnv(env)
+  var conf = getBabelAssetconf(env)
+  var compiler = webpack(conf)
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        console.log(colors.bgRed(`[task ${leftPad('babel-asset', 12)}]`), err)
+        reject()
+      } else {
+        console.log(colors.bgGreen(`[task ${leftPad('babel-asset', 12)}]`), stats.toString('normal'))
+        resolve()
+      }
+    })
+  })
 }
---
+++ scripts/webpack-conf/webpack-babel-asset-conf.js
@@ -0,0 +1,95 @@
+import webpack from 'webpack'
+
+import path from 'path'
+import glob from 'glob'
+
+import getDefinition from './webpack-definition'
+import getConfig from '../util/config'
+import babelrc from '../util/babelrc'
+
+var projectRoot = process.cwd()
+var contextPath = path.join(projectRoot, 'client')
+var assetRoot = path.join(contextPath, 'asset')
+var cliRoot = path.join(__dirname, '../../')
+
+function getEntry() {
+  var ret = {}
+  var entries = glob.sync(assetRoot + '/**/*.bl.js', {
+    cwd: assetRoot
+  })
+  entries.forEach((it) => {
+    var filePath = path.relative(assetRoot, it)
+    var entryName = filePath.slice(0, -6).replace(/\\/g, '/')
+    ret[entryName] = './asset/' + filePath
+  })
+  return ret
+}
+
+export default (env) => {
+  var config = getConfig(env)
+  var definition = getDefinition(env)
+  var conf = {
+    context: contextPath,
+    stats: {
+      children: false
+    },
+    cache: false,
+    devtool: env === 'development' ? '#source-map' : false,
+    entry: getEntry(),
+    output: {
+      filename: '[name].js?[chunkhash:7]',
+      chunkFilename: '[name].js?[chunkhash:7]',
+      path: path.join(projectRoot, 'client/dist/static'),
+      publicPath: path.join(config.client.publicPath, '/').replace(/\\/g, '/').replace(/\:\/([^\/])/i, '://$1')
+    },
+    resolve: {
+      extensions: ['', '.js'],
+      root: [path.join(projectRoot, 'node_modules')],
+      fallback: [path.join(cliRoot, 'node_modules')]
+    },
+    resolveLoader: {
+      root: [path.join(cliRoot, 'node_modules')],
+      fallback: [path.join(projectRoot, 'node_modules')]
+    },
+    module: {
+      preLoaders: [{
+        test: /\.(js|vue)$/,
+        loader: 'eslint',
+        include: assetRoot,
+        exclude: /node_modules/
+      }],
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: babelrc
+      }, {
+        test: /\.js$/,
+        loader: 'es3ify',
+        include: /node_modules/
+      }]
+    },
+    eslint: {
+      configFile: path.join(projectRoot, '.eslintrc.js'),
+      formatter: require('eslint-friendly-formatter')
+    },
+    babel: babelrc,
+    plugins: [
+      new webpack.IgnorePlugin(/vertx/),
+      new webpack.DefinePlugin(definition),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: false
+        }
+      })
+    ].concat(config.webpack.banner ?
+      new webpack.BannerPlugin(config.webpack.banner + ' | built at ' + new Date(config.version), {
+        entryOnly: true
+      }) : [])
+  }
+  return conf
+}
