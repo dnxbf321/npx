1718136907ec4accfe14e508c858f6afa30448f6
dengjiayao <dnxbf321@gmail.com>
2017-12-08 15:47:34 +0800

    环境变量是数组类型

--- scripts/webpack-conf/webpack-definition.js
+++ scripts/webpack-conf/webpack-definition.js
@@ -2,19 +2,12 @@ import getConfig from '../util/config'
 
 function formatDefinitions(obj) {
   const dealKV = (vObj, comb) => {
-    Object.keys(vObj).forEach((k) => {
+    Object.keys(vObj).forEach(k => {
       let v = vObj[k]
       let kComb = comb ? [comb, k].join('.') : k
-      switch (Object.prototype.toString.call(v)) {
-        case '[object Number]':
-        case '[object String]': // define plugin 把 string 当做可执行代码
-          obj[kComb] = JSON.stringify(v)
-          break
-        case '[object Object]':
-          dealKV(v, kComb)
-          obj[kComb] = JSON.stringify(kComb + ' is not a leaf node, you can use like \`' + kComb + '.' + Object.keys(v)[0] + '\`')
-          break
-        default:
+      obj[kComb] = JSON.stringify(v)
+      if (Object.prototype.toString.call(v) === '[object Object]') {
+        dealKV(v, kComb)
       }
     })
   }
@@ -22,7 +15,7 @@ function formatDefinitions(obj) {
   return obj
 }
 
-export default (env) => {
+export default env => {
   var envConfig = getConfig(env, 'isDefinition')
   envConfig = formatDefinitions(envConfig)
   return envConfig
